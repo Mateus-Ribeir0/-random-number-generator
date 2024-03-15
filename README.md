1b1bce4bfcf91ad585ef32539abe646b7429245d
Mateus Ribeiro <160142915+Mateus-Ribeir0@users.noreply.github.com>
2024-03-15 13:00:55 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# 🔢 Sorteador de Números 🎲
+
+## Sobre
+Este projeto, desenvolvido como parte do curso de lógica de programação, permite que você defina a quantidade de números a serem sorteados e o intervalo dentro do qual os números serão selecionados.
+
+### Funcionalidades
+- Escolha da quantidade de números a serem sorteados.
+- Definição do intervalo para o sorteio.
+- Geração de números aleatórios dentro do intervalo especificado.
+
+## Tecnologias
+![HTML](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white)
+![CSS](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
---
+++ app.js
@@ -0,0 +1,66 @@
+const sortear = () => {
+    // Capturar a qtd de números sorteados bem como de e ate.
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    
+    // Gerar condicional para informar situações onde não é possível sortear.
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
+    
+    if (quantidade > (ate - de)) {
+        alert('A quantidade de números sorteados é maior que a diferença entre os valores, tente novamente.');
+        return;
+    }    
+    
+    // Fazer uma lista com os números sorteados gerados aleatoriamente.
+    let sorteados = [];
+    let numero;
+    
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        while (sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numero);
+    }
+
+    // Exibir no HTML quais foram os números sorteados.
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+
+    alterarStatusBotao();
+};
+
+
+// Sortear número aleatório
+
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Habilitar e desabilitar o botão reiniciar.
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+
+    if (botao.classList.contains('container__botao-desabilitado')) {
+            botao.classList.remove('container__botao-desabilitado');
+            botao.classList.add('container__botao');
+    } else {
+            botao.classList.remove('container__botao');
+            botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+// Limpar informações para reiniciar.
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    resultado.innerHTML =  `<label class="texto__paragrafo">Números sorteados: Nenhum até o momento.</label>`
+    alterarStatusBotao()
+}
